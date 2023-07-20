112cf8fb627241bb3f2f7451905756f74462d083
paul-tes <pauasca8462@gmail.com>
2023-07-20 16:34:44 +0300

    fix robocop parantesis style

--- my_list.rb
+++ my_list.rb
@@ -18,12 +18,10 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-p list.all? { |e| e < 5 } #=> true
-p list.all? { |e| e > 5 } #=> false
+puts(list.all? { |e| e < 5 }) #=> true
+puts(list.all? { |e| e > 5 }) #=> false
 
-# Test #any?
-p list.any? { |e| e == 2 } #=> true
-p list.any? { |e| e == 5 } #=> false
-
+puts(list.any? { |e| e == 2 }) #=> true
+puts(list.any? { |e| e == 5 }) #=> false
 # Test #filter
 p list.filter(&:even?) #=> [2, 4]
